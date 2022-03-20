a160496630bbb1fea92426de2b9825121189382b
unknown <mashoodurrehmanofficial.com>
2022-03-21 00:36:39 +0500

    lastest

--- root/models.py
+++ root/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 import datetime
 from django.conf import settings
-
+from pytz import timezone 
+from datetime import  datetime
+from  django.utils.timezone import  now
 # Create your models here.
 
 
 class DriverTable(models.Model):
     name = models.CharField(max_length=500,default='',blank=True)
     taxi_number = models.CharField(max_length=500,default='',blank=True)
-    balance = models.IntegerField(default=0,blank=True)
+    balance = models.FloatField(default=0,blank=True)
+    owed = models.FloatField(default=0,blank=True)
     is_rent_deducted_today = models.BooleanField(default=False)
     is_paid_today = models.BooleanField(default=False)
-    last_rent_deduction_date = models.DateField( default=datetime.date.today)
-    last_paid_date = models.DateField( default=datetime.date.today,)
+    last_rent_deduction_date = models.DateField( null=True,blank=True,default=now)
+    last_paid_date = models.DateField(null=True,blank=True,default=now)
+    
+    
 
     
     
--- root/views.py
+++ root/views.py
@@ -1,3 +1,5 @@
+from time import timezone
+import pytz
 from django.shortcuts import redirect, render
 from django.http import JsonResponse
 from .models import *
@@ -5,12 +7,24 @@ from django.views.decorators.csrf import csrf_exempt
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login,logout
+from pytz import timezone 
 
 
 
-
-
-
+def synchronizeDriverOwedAmount():
+    available_drivers = DriverTable.objects.all()
+    for driver in available_drivers:
+        # print("-"*10)
+        today_date = datetime.now(timezone(settings.TIME_ZONE)).date()
+        last_rent_deduction_date = driver.last_rent_deduction_date 
+        
+        days = (today_date - last_rent_deduction_date).days
+        
+        owed_amount = int(days)*60
+        driver.owed = owed_amount
+        driver.save()
+        
+        # print(days)
  
 
 
@@ -21,19 +35,26 @@ def index(request):
     context = {'title':"Driver Listing"}
     available_drivers = DriverTable.objects.all()
     
+    synchronizeDriverOwedAmount()
+         
+        
+        
     
+    available_drivers = DriverTable.objects.all()
     
-    today_date = datetime.now(). strftime("%m/%d/%Y")
+    today_date = datetime.now(timezone(settings.TIME_ZONE)). strftime("%m/%d/%Y")
     context['today_date'] = today_date
     context['available_drivers'] = available_drivers
     return render(request, 'root/index.html',context)
 
  
 def edit_driver(request,id=None):
+    
     if request.method=="POST":
         name = request.POST['name']
         taxi_number = request.POST['taxi_number']
         balance = request.POST['balance']
+        owed = request.POST['owed']
         required_driver = None
         if id:
             required_driver = DriverTable.objects.get(id=id)
@@ -43,20 +64,27 @@ def edit_driver(request,id=None):
         required_driver.name = name
         required_driver.taxi_number = taxi_number
         required_driver.balance = balance
+        required_driver.owed = owed
         required_driver.save()
         return redirect("/")
     
     
     
     context = {'title':"Edit Driver Data" if id else "Add Driver"} 
+    synchronizeDriverOwedAmount()
+    
     required_driver = DriverTable.objects.filter(id=id)
     if required_driver:
         required_driver = required_driver.first()
-    context['required_driver'] = required_driver
-    today_date = datetime.now(). strftime("%m/%d/%Y")
+    context['required_driver'] = required_driver 
+    today_date = datetime.now(timezone(settings.TIME_ZONE)). strftime("%m/%d/%Y")
     context['today_date'] = today_date
     
-    print(required_driver)
+     
+    
+    now_time = datetime.now(timezone(settings.TIME_ZONE)). strftime("%m/%d/%Y")
+    
+    print(now_time)
     
     
     return render(request, 'root/edit_driver.html',context)
