c597adc45d9e1bdc783fb36ac9edf9619446fc24
unknown <mashoodurrehmanofficial.com>
2022-03-20 05:49:58 +0500

    first commit

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -108,11 +108,11 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
-USE_I18N = False
+USE_I18N = True
 
-USE_L10N = False
+USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
